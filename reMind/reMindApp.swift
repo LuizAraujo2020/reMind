490ca879dac7052be074e8d9d19244014cffc774
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2024-02-15 17:18:40 -0300

    Initial Commit

---
+++ reMind/reMindApp.swift
@@ -0,0 +1,17 @@
+//
+//  reMindApp.swift
+//  reMind
+//
+//  Created by Luiz Araujo on 15/02/24.
+//
+
+import SwiftUI
+
+@main
+struct reMindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
